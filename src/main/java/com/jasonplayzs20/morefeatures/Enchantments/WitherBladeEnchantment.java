c6517beef131e155d6071c7a57e4ffe622a4caa9
JasonPlayzs20 <103009819+JasonPlayzs20@users.noreply.github.com>
2023-07-31 20:02:15 -0400

    new enchantment yay

---
+++ src/main/java/com/jasonplayzs20/morefeatures/Enchantments/WitherBladeEnchantment.java
@@ -0,0 +1,55 @@
+package com.jasonplayzs20.morefeatures.Enchantments;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class WitherBladeEnchantment extends Enchantment implements Listener {
+    public WitherBladeEnchantment() {
+        super(NamespacedKey.minecraft("wither_blade"));
+    }
+
+    @Override
+    public String getName() {
+        return "Wither Blade";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 4;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.WEAPON;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment enchantment) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack itemStack) {
+        return false;
+    }
+}
